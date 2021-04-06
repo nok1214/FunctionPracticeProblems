ef0063b39d8157bd7246cc1188c5e272397717bd
nok1214 <80727744+nok1214@users.noreply.github.com>
2021-04-06 12:56:34 -0700

    updated

--- Problem5.js
+++ Problem5.js
@@ -13,6 +13,7 @@ step 4: use "if" statement to give condition for how our program should run;
 */
 
 //code starts here
+//With 3 condition
 
 for(var i=1; i <= 100; i++){
     if (i % 3 ===0 && i % 5 ===0){                  //the condition is going on top because checking this first so that when the program met other condition before this will not get skipped;
@@ -26,3 +27,46 @@ for(var i=1; i <= 100; i++){
     }
     else console.log(i);                            //if none of the condition met above, comtinue counting;
 }
+
+
+
+//version 2: combine 3 and 5 together;
+
+
+for(var i = 1; i <= 100; i++){
+  if(i % 15 ===0){
+    console.log("FizzBuzz");
+  }
+  else if(i % 3 ===0){
+    console.log("Fizz");
+  }
+  else if(i % 5 ===0){
+    console.log("Buzz");
+  }
+  else console.log(i);
+}
+
+
+//version 3: function method
+
+function fizzBuzz(i) {
+  var word = ""                     //set output as one of the variable(word) with an empty string;
+
+  if(i % 3 ===0)                    // first condition if true, "Fizz" will be our string; If false, output i;
+  word += 'Fizz'
+
+  if(i % 5 ===0)                    //second condition if true, "Buzz" will be our string; If false, output i;
+  word += 'Buzz'  
+
+  if(i >= 100)                      //tell the function to stop when i reach to 100;
+  return
+
+  console.log(word || i)           //log the output if one of condition is true by using "||";
+
+  fizzBuzz(i++)
+  
+}
+fizzBuzz(1);
+
+
+
