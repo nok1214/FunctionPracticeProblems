c1d3b0469c0adc22a31ed03a36ad721604a936f2
nok1214 <80727744+nok1214@users.noreply.github.com>
2021-04-07 12:29:20 -0700

    fixed variable name to describe what exactly is inside, changed to log out the function.

--- Problem1.js
+++ Problem1.js
@@ -17,11 +17,11 @@ step 7: run the function with a given string;
 
 function findLongestWord(str) {
     var splitString = str.split(' ');                                   // split string into a new arry of string by space;
-    var theLongestWord = 0;                                             // declear a new variable to store the longest word, which start with 0;
+    var theLongestWordCount = 0;                                        // declear a new variable to store the longest word, which start with 0;
     var longestWord = ''                                                //set the output value to a string where it will show the longest word in our string;
     for(var i=0; i< splitString.length; i++) { 
-        if(splitString[i].length > theLongestWord) {                    // check if first word inside splitString's length is greather than the previous value stored inside theLongestWord(0) variable;
-            theLongestWord = splitString[i].length;                     // if is true, theLongestWord will have a new value;
+        if(splitString[i].length > theLongestWordCount) {               // check if first word inside splitString's length is greather than the previous value stored inside theLongestWord(0) variable;
+            theLongestWordcount = splitString[i].length;                // if is true, theLongestWord will have a new value;
             longestWord = splitString[i];                               //set our longest word equal to the value that matches the condition we are looking for and store it in variable longestWord;
         }
         
@@ -29,7 +29,7 @@ function findLongestWord(str) {
     return longestWord;                                                 //return the longest word instead of number;
 }
 
-findLongestWord('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.');// output 'consectetur'
+console.log(findLongestWord('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'));// output 'consectetur'
 
 
 //using forEach() to find the longest word;
@@ -37,35 +37,35 @@ findLongestWord('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
 
 function findLongestWord(str){
     var splitString = str.split(' ');                                   // split string into a new arry of string by space;
-    var theLongestWord = 0;                                             // declear a new variable to store the longest word, which start with 0;
+    var theLongestWordCount = 0;                                        // declear a new variable to store the longest word, which start with 0;
     var longestWord = '';                                               //set the output value to a string where it will show the longest word in our string;
 
     splitString.forEach(function (word){                                //use .forEach() on splitString variable since that is where we are looking for the longest word; set "word" as .forEach() value;
-        if(word.length > theLongestWord){                               //use "if" statement to check if word.length is greater than variable theLongestWord which start with 0;
-            theLongestWord = word.length;                               // if is true, theLongestWord will have a new value;
+        if(word.length > theLongestWordCount){                          //use "if" statement to check if word.length is greater than variable theLongestWord which start with 0;
+            theLongestWordCount = word.length;                          // if is true, theLongestWord will have a new value;
             longestWord = word;                                         //set our longest word equal to the value that matches the condition we are looking for and store it in variable longestWord;
         }
     });
     return longestWord;                                                 //return the longest word;
 }
 
-findLongestWord('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.');// output 'consectetur'
+console.log(findLongestWord('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'));// output 'consectetur'
 
 //using map() to find the longest word;
 //code starts here;
 
 function findLongestWord(str){
     var splitString = str.split(' ');                                   // split string into a new arry of string by space;
-    var theLongestWord = 0;                                             // declear a new variable to store the longest word, which start with 0;
+    var theLongestWordCount = 0;                                        // declear a new variable to store the longest word, which start with 0;
     var longestWord = '';                                               //set the output value to a string where it will show the longest word in our string;
 
     splitString.map(function (word){                                    //use .map() on splitString variable since that is where we are looking for the longest word; set "word" as .map() value;
-        if(word.length > theLongestWord){                               //use "if" statement to check if word.length is greater than variable theLongestWord which start with 0;
-            theLongestWord = word.length;                               // if is true, theLongestWord will have a new value;
+        if(word.length > theLongestWordCount){                          //use "if" statement to check if word.length is greater than variable theLongestWord which start with 0;
+            theLongestWordCount = word.length;                          // if is true, theLongestWord will have a new value;
             longestWord = word;                                         //set our longest word equal to the value that matches the condition we are looking for and store it in variable longestWord;
         }
     });
     return longestWord;                                                 //return the longest word;
 }
 
-findLongestWord('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.');// output 'consectetur'
+console.log(findLongestWord('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'));// output 'consectetur'
