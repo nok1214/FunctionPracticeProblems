0a6d24878a9f8b48de516fae67b66e74e0667acb
nok1214 <80727744+nok1214@users.noreply.github.com>
2021-04-06 18:25:07 -0700

    added .forEach(), .map(), and .filter() method with using .indexOf() and .includes()to count vowels in a string.

--- Problem2.js
+++ Problem2.js
@@ -15,18 +15,91 @@ step 7: return the value at the end of the function;
 
 */
 
-// code starts here
+//code starts here
 
 function numberOfVowels(str) {
-    var splitString = str.split(",");           //spliting string into individual letters;
-    const vowels = ['a', 'e', 'i', 'o', 'u'];   // define what vowels are;
-    var numofVowels = 0;                           //start the count at 0 or at the beginning of the string;
+    var splitString = str.split("");                                //spliting string into individual letters;
+    const vowels = ['a', 'e', 'i', 'o', 'u'];                       // define what vowels are;
+    var numOfVowels = 0;                                            //start the count at 0;
      for(let char of str) { 
-         if (vowels.includes(char)) {           //compare the characters in our string by using .include method;
-             numOfVowels++;                        //continue counting using increment;
+         if (vowels.includes(char.toLowerCase())) {                 //compare the characters in our string by using .include method, and use .toLowerCase() in case there are capital letters inside the string;
+             numOfVowels++;                                         //continue counting using increment;
          }
      }
-    return numOfVowels;                            //return the number of vowels in our string;
+    console.log(numOfVowels);                                       //log the number of vowels in our string;
 }
 
 numberOfVowels('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'); // output 45;
+
+
+//using .forEach() and .indexOf() to find vowels in a string;
+//code starts here
+
+function numberOfVowels(str) {
+    var splitString = str.split('');                                //spliting string into individual letters;
+    const vowels = ['a', 'e', 'i', 'o', 'u'];                       // define what vowels are;
+    var numOfVowels = 0;                                            //start the count at 0;
+    
+    splitString.forEach(function (vowelCounts) {                    //using .forEach on the variable we just decleared with vowelCounts as our function value;
+        if(vowels.indexOf(vowelCounts.toLowerCase()) !== -1){       //use .indexOf() to return the position of the value that matches const vowels, and use .toLowerCase() in case there are capital letters inside the string; 
+            numOfVowels++;                                          //continue counting using increment;
+        }
+    });
+    console.log(numOfVowels);                                       //log the number of vowels in our string;
+}
+
+numberOfVowels('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.');// output 45;
+
+//using .map() and .indexOf() to find vowels in a string;
+//code starts here
+
+function numberOfVowels(str) {
+    var splitString = str.split('');                                //spliting string into individual letters;
+    const vowels = ['a', 'e', 'i', 'o', 'u'];                       // define what vowels are;
+    var numOfVowels = 0;                                            //start the count at 0;
+    
+    splitString.map(function (vowelCounts) {                        //using .map() on the variable we just decleared with vowelCounts as our function value;
+        if(vowels.indexOf(vowelCounts.toLowerCase()) !== -1){       //use .indexOf() to return the position of the value that matches const vowels, and use .toLowerCase() in case there are capital letters inside the string; 
+            numOfVowels++;                                          //continue counting using increment;
+        }
+    });
+    console.log(numOfVowels);                                       //log the number of vowels in our string;
+}
+
+numberOfVowels('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.');// output 45;
+
+//using .filter() and .indexOf() to find vowels in a string;
+//code starts here
+
+function numberOfVowels(str) {
+    var splitString = str.split('');                                //spliting string into individual letters;
+    const vowels = ['a', 'e', 'i', 'o', 'u'];                       // define what vowels are;
+    var numOfVowels = 0;                                            //start the count at 0;
+    
+    splitString.filter(function (vowelCounts) {                     //using .filter() on the variable we just decleared with vowelCounts as our function value;
+        if(vowels.indexOf(vowelCounts.toLowerCase()) !== -1){       //use .indexOf() to return the position of the value that matches const vowels, and use .toLowerCase() in case there are capital letters inside the string; 
+            numOfVowels++;                                          //continue counting using increment;
+        }
+    });
+    console.log(numOfVowels);                                       //log the number of vowels in our string;
+}
+
+numberOfVowels('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.');// output 45;
+
+//using .forEach() and .includes() to find vowels in a string;
+//code starts here
+
+function numberOfVowels(str) {
+    var splitString = str.split('');                                //spliting string into individual letters;
+    const vowels = ['a', 'e', 'i', 'o', 'u'];                       // define what vowels are;
+    var numOfVowels = 0;                                            //start the count at 0;
+    
+    splitString.forEach(function (vowelCounts) {                    //using .forEach() on the variable we just decleared with vowelCounts as our function value;
+        if(vowels.includes(vowelCounts.toLowerCase())){             //compare the characters in our string by using .include method, and use .toLowerCase() in case there are capital letters inside the string;
+            numOfVowels++;                                          //continue counting using increment;
+        }
+    });
+    console.log(numOfVowels);                                       //log the number of vowels in our string;
+}
+
+numberOfVowels('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.');// output 45;
